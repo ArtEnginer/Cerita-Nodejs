41616d5ab7400e73a32c9c40e26ddd792df78faa
angga saputra <anggaegae@gmail.com>
2021-02-02 12:56:37 +0700

    uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI = 'mongodb://admin:admin123@cluster0-shard-00-01.c3vtd.mongodb.net:27017/compose?authSource=admin&ssl=true', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
